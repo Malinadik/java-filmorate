167d9d10bb4253a2382652f6cce6a89fe378ed53
Marina <copperraspberry@yandex.ru>
2023-04-03 13:18:11 +0300

    fix: доработать метод обращения к БД, изменить структуру кода

--- src/main/java/ru/yandex/practicum/filmorate/controllers/MpaController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/MpaController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.filmorate.model.MPARating;
-import ru.yandex.practicum.filmorate.storage.dao.FilmDao;
 import ru.yandex.practicum.filmorate.storage.dao.MpaDao;
 
 import java.util.List;
